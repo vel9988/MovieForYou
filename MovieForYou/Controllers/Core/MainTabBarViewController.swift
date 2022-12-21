4e9627c0514e9ca7b881113382befd5a9706b889
Dmitriy <58105647+vel9988@users.noreply.github.com>
2022-12-21 17:51:40 +0600

    Начало

---
+++ MovieForYou/Controllers/Core/MainTabBarViewController.swift
@@ -0,0 +1,28 @@
+//
+//  MainTabBarViewController.swift
+//  MovieForYou
+//
+//  Created by Dmitryi Velko on 21.12.2022.
+//
+
+import UIKit
+
+class MainTabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemRed
+        
+    }
+    
+    private func setupViewControllers() {
+        let vc1
+        let vc2
+        let vc3
+        let vc4
+    }
+
+
+}
+
