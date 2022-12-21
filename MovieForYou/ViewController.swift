6e70eaca1acaf9450f15296ec08a28c83d10dca1
Dmitriy <58105647+vel9988@users.noreply.github.com>
2022-12-21 14:06:02 +0600

    Initial Commit

---
+++ MovieForYou/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieForYou
+//
+//  Created by Dmitryi Velko on 21.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
